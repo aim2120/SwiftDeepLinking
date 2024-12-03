d5e84ace9547c6989a4b21c2d17a86ed80db184b
Annalise Mariottini <annalise.mariottini@columbia.edu>
2024-12-03 08:27:50 -0500

    allow for library development from Project.swift

--- SwiftDeepLinkingExample/Project.swift
+++ SwiftDeepLinkingExample/Project.swift
@@ -4,10 +4,58 @@ private func bundleID(_ name: String) -> String {
     "com.aim2120.\(name)"
 }
 
+extension Set<Destination> {
+    fileprivate static let libraryDestinations: Self = Set(Destination.allCases)
+}
+
+extension DeploymentTargets {
+    fileprivate static let libraryDeploymentTargets: Self = .multiplatform(
+        iOS: "17.0",
+        macOS: "14.0",
+        watchOS: "10.0",
+        tvOS: "17.0",
+        visionOS: "1.0"
+    )
+}
+
+extension Target {
+    fileprivate static func libraryTarget(
+        name: String,
+        dependencies: [TargetDependency] = []
+    ) -> Target {
+        .target(
+            name: name,
+            destinations: .libraryDestinations,
+            product: .staticLibrary,
+            bundleId: bundleID(name),
+            deploymentTargets: .libraryDeploymentTargets,
+            sources: ["../Sources/\(name)/**"],
+            dependencies: dependencies
+        )
+    }
+
+    fileprivate static func testTarget(
+        testing name: String,
+        dependencies: [TargetDependency] = []
+    ) -> Target {
+        .target(
+            name: "\(name)Tests",
+            destinations: .libraryDestinations,
+            product: .unitTests,
+            bundleId: bundleID("\(name)Tests"),
+            deploymentTargets: .libraryDeploymentTargets,
+            sources: ["../Tests/\(name)Tests/**"],
+            dependencies: [
+                .target(name: name),
+            ] + dependencies
+        )
+    }
+}
+
 let project = Project(
     name: "SwiftDeepLinkingExample",
     packages: [
-        .local(path: ".."),
+        .remote(url: "https://github.com/aim2120/SwiftParameterPackExtras.git", requirement: .upToNextMajor(from: "0.1.0")),
     ],
     targets: [
         .target(
@@ -26,8 +74,33 @@ let project = Project(
             sources: ["SwiftDeepLinkingExample/Sources/**"],
             resources: ["SwiftDeepLinkingExample/Resources/**"],
             dependencies: [
-                .package(product: "SwiftDeepLinking"),
-                .package(product: "SwiftDeepLinkingSwiftUI"),
+                .target(name: "SwiftDeepLinking"),
+                .target(name: "SwiftDeepLinkingSwiftUI"),
+            ]
+        ),
+        .libraryTarget(
+            name: "SwiftDeepLinking",
+            dependencies: [
+                .package(product: "SwiftParameterPackExtras"),
+            ]
+        ),
+        .libraryTarget(
+            name: "SwiftDeepLinkingSwiftUI",
+            dependencies: [
+                .package(product: "SwiftParameterPackExtras"),
+                .target(name: "SwiftDeepLinking"),
+            ]
+        ),
+        .testTarget(
+            testing: "SwiftDeepLinking",
+            dependencies: [
+                .package(product: "SwiftParameterPackExtras"),
+            ]
+        ),
+        .testTarget(
+            testing: "SwiftDeepLinkingSwiftUI",
+            dependencies: [
+                .package(product: "SwiftParameterPackExtras"),
             ]
         ),
     ]
